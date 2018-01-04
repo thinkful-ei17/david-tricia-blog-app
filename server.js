e7eca42cd9724c66085d2d0ba525b712cfe347ac
TriciaF <TriciaF@thinkful.com>
2018-01-04 14:54:39 -0500

    completed CRUD and Knex operations

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const storiesRouter = require('./routers/stories-router');
-const blog = require('./server');
+
 const { PORT } = require('./config');
 const { DATABASE } = require('./config');
 const app = express();
@@ -17,9 +17,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 
-app.use('/api/v0', blog);
 app.use('/api/v1', storiesRouter);
-app.use('/api/v1', storiesRouter);
+app.use('/api/v2', storiesRouter);
+
+
+app.get('/api/v0', (req, res) => {
+  console.log('enter app.get');
+  knex.select('id', 'title', 'content')
+    .from('stories')
+    .orderBy('id', 'asc')
+    .then(results => {
+      res.status(200).json(results);
+    });
+});
+
+app.get('/api/v0/:id', (req, res) => {
+  knex.select('id', 'title', 'content')
+    .from('stories')
+    .where('stories.id', req.params.id)
+    .orderBy('id', 'asc')
+    .then(results => {
+      res.status(200).json(results);
+    });
+});
+
+app.post('/api/v0', jsonParser, (req, res) => {
+  knex
+    .insert([{ title: req.body.title, content: req.body.content }]).into('stories')
+    .then(results => {
+      res.status(201).json(results);
+    });
+});
+
+app.put('/api/v0/:id', (req, res) => {
+  knex.select('title', 'content')
+    .from('stories')
+    .where('stories.id', req.params.id)
+    .returning('id', 'title', 'content')
+    .update({
+      title: req.body.title,
+      content: req.body.content
+    })
+    .then(results => {
+      res.status(201).json(results);
+    });
+});
+
+app.delete('/api/v0/:id', (req, res) => {
+  knex('stories')
+    .where('id', req.params.id)
+    .del()
+    .then(results => {
+      res.status(204).json(results);
+    });
+});
+
 
 // Catch-all endpoint for requests to non-existent endpoint
 app.use(function(req, res, next) {
@@ -44,58 +96,3 @@ const server = app.listen(PORT, () => {
   .on('error', err => {
     console.error(err);
   });
-
-
-app.get('/api/v0', (req, res) => {
-  knex.select('id', 'title', 'content')
-    .from('stories')
-    .then(results => {
-      res.json(results);
-      res.sendStatus(200);
-    });
-});
-
-app.get('/api/v0/:id', (req, res) => {
-  knex.select('id', 'title', 'content')
-    .from('stories')
-    .where('stories.id', req.params.id)
-    .then(results => {
-      res.json(results);
-      res.sendStatus(200);
-    });
-});
-
-app.post('/api/v0', jsonParser, (req, res) => {
-  knex
-    .insert([{ title: req.body.title, content: req.body.content }]).into('stories')
-    .then(results => {
-      res.json(results);
-      res.sendStatus(201);
-    });
-});
-
-app.put('/api/v0/:id', (req, res) => {
-  knex.select('title', 'content')
-    .from('stories')
-    .where('stories.id', req.params.id)
-    .returning('id', 'title', 'content')
-  // .insert([{title: req.body.title}, {content: req.body.content}])
-    .update({
-      title: req.body.title,
-      content: req.body.content
-    })
-    .then(results => {
-      res.json(results);
-      res.sendStatus(201);
-    });
-});
-
-app.delete('/api/v0/:id', (req, res) => {
-  knex('stories')
-    .where('id', req.params.id)
-    .del()
-    .then(results => {
-      res.json(results);
-      res.sendStatus(204);
-    });
-});
