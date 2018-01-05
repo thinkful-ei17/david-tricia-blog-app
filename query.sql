eba438dfa070f6a56e1d5a6de8eaf5407ee73069
John Castillo <JohnluisCastillo@gmail.com>
2018-01-05 12:44:21 -0500

    added author db's

--- query.sql
+++ query.sql
@@ -4,14 +4,17 @@ DROP TABLE IF EXISTS authors;
 CREATE TABLE stories(
   id serial PRIMARY KEY,
   title text NOT NULL,
-  content text
+  content text,
+  author_id int REFERENCES authors ON DELETE RESTRICT,
 );
 
 CREATE TABLE authors(
   id serial PRIMARY KEY,
-  author text NOT NULL,
+  email text NOT NULL,
+  username text NOT NULL 
 );
 
-INSERT INTO stories(title, content) VALUES ('Local Restaurant Reviews', 'Cafe Ole serves very good food'),('Music Reviews', 'Must listen to the new Cold Play CD'),('Movie Reviews', 'The new Star Wars movie is great'),('Government opinions', 'President Trump is a scary person');
+INSERT INTO stories(title, content,author_id) VALUES 
+('Local Restaurant Reviews', 'Cafe Ole serves very good food','Maki'),('Music Reviews', 'Must listen to the new Cold Play CD','Coldplay'),('Movie Reviews', 'The new Star Wars movie is great','George Lucas'),('Government opinions', 'President Trump is a scary person','Trump');
 
 SELECT * FROM stories;
